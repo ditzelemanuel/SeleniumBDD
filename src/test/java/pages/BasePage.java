31db8765cbe2949a9845dca558c7813d0500fca8
Emanuel Ditzel <ditzelemanuel@gmail.com>
2024-07-06 01:28:13 -0300

    add utility methods to BasePage

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -1,10 +1,79 @@
 package pages;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BasePage {
 
     protected static WebDriver driver;
 
+    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+
+    static {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+    }
+
+    public BasePage(WebDriver driver){
+        BasePage.driver = driver;
+    }
+
+    public static void navigateTo(String url){
+        driver.get(url);
+    }
+
+    public static void closeBrowser(){
+        driver.quit();
+    }
+
+    private WebElement find(String locator){
+        return wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(locator)));
+    }
+
+    public void clickElement(String locator){
+        find(locator).click();
+    }
+
+    public void write(String locator, String keysToSend){
+        find(locator).clear();
+        find(locator).sendKeys(keysToSend);
+    }
+
+    public void selectDropdownByValue(String locator, String value){
+        Select dropdown = new Select(find(locator));
+        dropdown.selectByValue(value);
+    }
+
+    public void selectDropdownByIndex(String locator, int index){
+        Select dropdown = new Select(find(locator));
+        dropdown.selectByIndex(index);
+    }
+
+    public int dropdownSize(String locator){
+        Select dropdown = new Select(find(locator));
+        List<WebElement> elements = dropdown.getOptions();
+        return elements.size();
+    }
+
+    public List<String> getDropdownValues(String locator){
+        Select dropdown = new Select(find(locator));
+        List<WebElement> dropdownOptions = dropdown.getOptions();
+        List<String> values = new ArrayList<>();
+
+        for (WebElement option : dropdownOptions) {
+            values.add(option.getText());
+        }
+        return values;
+    }
 }
