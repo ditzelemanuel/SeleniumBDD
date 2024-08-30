cac94621f972e4e4832596031a20f05caf1b7d8c
Emanuel Ditzel <ditzelemanuel@gmail.com>
2024-08-29 22:52:11 -0300

    Refactor Cucumber plugin

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/resources",
         glue = "steps",
-        plugin = {"pretty", "html:target/cucumber-reports", "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"})
+        plugin = {"pretty", "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"})
 
 public class TestRunner {
 
